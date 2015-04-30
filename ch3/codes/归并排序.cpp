654fecdf9577c7b135992b4128a17f6d827a17d3
vjudge1 <381062331@qq.com>
2015-04-30 12:25:56 +0800

    搬运了一点点

---
+++ ch3/codes/归并排序.cpp
@@ -0,0 +1,19 @@
+int temp[N];
+void mergeSort(int *start, int *end)
+{
+    if (start+1>=end) return;
+    // 划分阶段、递归
+    int *mid = start+(end-start)/2;
+    mergeSort(start, mid);
+    mergeSort(mid, end);
+    
+    // 将 mid 两侧的两个有序表合并为一个有序表。
+    int *p=start,*q=mid,*r=temp;
+    while (p<mid || q<end)
+        if (q>=end || (p<mid && *p<=*q))
+            *(r++)=*(p++);
+        else
+            *(r++)=*(q++);
+            
+    for (p=start, r=temp; p<end; p++, r++) *p=*r;
+}
